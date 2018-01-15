f5c5e0b70d892e71fb47ad8c9dbe3e8eaf9fb3ca
chaozzj <35468971+chaozzj@users.noreply.github.com>
2018-01-15 15:17:13 -0600

    update
    asdsad

--- views/layouts/default/header.php
+++ views/layouts/default/header.php
@@ -74,7 +74,7 @@
 </div>
 <div id="content">
     <div id="content-header">
-        <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
+        <!--<div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>-->
     </div>
     <div class="container-fluid">
         <div class="quick-actions_homepage">
